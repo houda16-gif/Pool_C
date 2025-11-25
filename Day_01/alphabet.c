5f07639ef2afe60ea0102ff88a7d1e32c20ed98f
houda16-gif <houdafg0@gmail.com>
2025-11-25 22:40:41 +0100

    hello

---
+++ Day_01/alphabet.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void print_alphabet(void){
+    char i;
+    for (i='a';i<='z';i++)
+    write(1,&i,1);
+}
+int main( {
+    print_alphabet();
+    return 0;
+})
--- Day_01/print.c
+++ Day_01/print.c
@@ -3,3 +3,6 @@ int main ()
 void print_hello (void) {
     printf("hello world");
 }
+
+
+
